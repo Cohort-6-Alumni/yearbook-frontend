0230cca1e09c58f8eb0866db6b7b71a57be33296
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-12-19 14:41:54 -0500

    feat: landing page, contributor card and basic carousel

---
+++ src/data/contributors.js
@@ -0,0 +1,84 @@
+
+export const contributors = [
+  {
+    id: 1,
+    name: 'Amyi',
+    cohort: 'Cohot 6',
+    role: 'Tech Enthusiast',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The academy was an eye opener for me . It taught me that impossible is nothing.',
+  },
+  {
+    id: 2,
+    name: 'Siphu',
+    cohort: 'Cohort 6',
+    role: 'AI geek',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f9e7c53cd2500184e6bafcf0d961136a4765d5eea788af0b71c58307af6fb681?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The academy was insightful to me, I encourage everyone moving to tech to apply for the bootcamp',
+  },
+  {
+    id: 3,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+  {
+    id: 4,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+  {
+    id: 5,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+  {
+    id: 6,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+  {
+    id: 7,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+  {
+    id: 8,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+  {
+    id: 9,
+    name: 'Emma',
+    cohort: 'Founder',
+    role: 'Debug guru',
+    image:
+      'https://cdn.builder.io/api/v1/image/assets/fdc3c41cbf0743359cb4456bd858c80a/f147697e9a4f8bd350f31d73a56c27e9e2519cf1fdfbc7737d3814b298b42544?apiKey=fdc3c41cbf0743359cb4456bd858c80a&',
+    text: 'The instructors are the best',
+  },
+];
