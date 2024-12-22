5d0646234658a836771f5a8f66627a2f14247e05
oleesir <oleesir@gmail.com>
2024-12-22 01:06:15 -0500

    update routing

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -1,11 +1,23 @@
+import { useContext } from "react";
+import {useNavigate} from "react-router";
 import instagramLogo from "../assets/instagram.png";
 import linkedin from "../assets/linkedin.png";
+import { AppContext } from "../context/contextApi.jsx";
 import PropTypes from "prop-types";
 
 
-const CustomCard = ({ bio,previousField,hobbies,instagram, firstName, lastName,picture }) => {
+const CustomCard = ({ bio,previousField,hobbies,instagram, firstName, lastName,picture,id }) => {
+    const navigate = useNavigate();
+    const {getUserProfilesCxt} = useContext(AppContext);
+    console.log("CUSTOM CARD",getUserProfilesCxt())
+
+    const handleClick = ()=>{
+    navigate(`/user_profile/${id}`)
+    }
+
+
   return (
-    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md p-4 cursor-pointer">
+    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-md p-4 cursor-pointer" onClick={handleClick} >
       <div className="flex items-center mb-2">
         <img
           className="w-[120px] h-[120px] rounded-full border border-gray-300"
@@ -74,4 +86,5 @@ CustomCard.propTypes = {
     firstName: PropTypes.string,
     lastName: PropTypes.string,
     picture: PropTypes.string,
+    id: PropTypes.string,
 };
--- src/pages/app/HomePage.jsx
+++ src/pages/app/HomePage.jsx
@@ -1,8 +1,11 @@
 import CustomCard from '../../components/CustomCard.jsx';
-import {useEffect, useState} from "react";
-import {getProfiles} from "../../api/index.js";
+import { useContext, useEffect, useState } from "react";
+import { getProfiles } from "../../api/index.js";
+import { AppContext } from "../../context/contextApi.jsx";
+import ProfileCard from "../../components/ProfileCard.jsx";
 
 const HomePage = () => {
+  const { setUserProfilesCxt } = useContext(AppContext);
   const [profiles, setProfiles] = useState([]);
 
 
@@ -11,6 +14,7 @@ const HomePage = () => {
       try {
         const res = await getProfiles();
         setProfiles(res?.data.content);
+        setUserProfilesCxt(res?.data.content)
       } catch (err) {
         console.error("Error fetching profiles:", err);
       }
@@ -19,23 +23,35 @@ const HomePage = () => {
     fetchProfiles();
   }, []);
 
-  console.log("PRO",profiles);
 
+console.log("PROFILES", profiles);
 
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {profiles.map((profile,index) => (
-            <CustomCard
-            key={index}
-            id={index}
-            bio={profile?.bio}
-            instagram={profile?.instagram}
-            picture={profile?.picture}
-            hobbies={profile?.hobbies}
-            previousField={profile?.previousField}
-            firstName={profile?.user.firstName}
-            lastName={profile?.user.lastName}
+        {/*{profiles.map((profile) => (*/}
+        {/*    <CustomCard*/}
+        {/*    key={profile?.profileId}*/}
+        {/*    id={profile?.profileId}*/}
+        {/*    bio={profile?.bio}*/}
+        {/*    instagram={profile?.instagram}*/}
+        {/*    picture={profile?.picture}*/}
+        {/*    hobbies={profile?.hobbies}*/}
+        {/*    previousField={profile?.previousField}*/}
+        {/*    firstName={profile?.user.firstName}*/}
+        {/*    lastName={profile?.user.lastName}*/}
+        {/*    />*/}
+        {/*))}*/}
+
+
+        {profiles.map((profile) => (
+            <ProfileCard
+                key={profile?.profileId}
+                id={profile?.profileId}
+                instagram={profile?.instagram}
+                picture={profile?.picture}
+                firstName={profile?.user.firstName}
+                lastName={profile?.user.lastName}
             />
         ))}
       </div>
